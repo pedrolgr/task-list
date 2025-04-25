b1d5f1394c1d2ea57b21601a3ce3161c4e7208e5
Pedro Guimaraes <pedrolgr47@gmail.com>
2025-04-24 22:24:06 -0300

    criação dos diretorios client e server e inicio da configuração do backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,29 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import TaskEditor from './components/TaskForm';
 import TaskList from './components/TaskList';
 import AddTask from './components/AddTask';
 import NewTaskButton from './components/NewTaskButton';
 
 import { TasksProvider } from './context/TasksContext';
+import Login from './pages/Login';
+import TaskApp from './pages/TaskApp';
 
 
 function App() {
 
   return (
-    <TasksProvider>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login />}/>
+        <Route path='/taskapp' element={<TaskApp />}/>
+      </Routes>
+    </Router>
+
+    /**<TasksProvider>
       <NewTaskButton />
       <TaskList />
-    </TasksProvider>
+    </TasksProvider> */
   );
 }
 
---
+++ client/src/pages/TaskApp.js
@@ -0,0 +1,10 @@
+const TaskApp = () => {
+
+    return (
+        <div>
+            <h1>TaskApp</h1>
+        </div>
+    )
+}
+
+export default TaskApp;
